9f82aed6e398fc9292abf0b0dd9054964ecaaeff
cznelson2005 <cznelson2005@gmail.com>
2015-08-09 21:41:42 +0200

    revise data script

--- Data_script.R
+++ Data_script.R
@@ -1,7 +1,14 @@
-#setwd("")
+setwd("D:/R/Plotting")
 library(lubridate)
 
+###read whole data set
 electric<-read.table("household_power_consumption.txt",sep = ";", dec = ".", na.strings = "?", header = TRUE)
+
+###cast Date column to date format
 electric$Date<-as.Date(electric$Date, "%d/%m/%Y")
+
+###Take only 2007/02/01~02 data set
 electric<-electric[electric$Date>="2007/02/01" & electric$Date<="2007/02/02",]
+
+###Create Date.Time column
 electric[, "Date.Time"]<-ymd(electric$Date)+hms(electric$Time)
